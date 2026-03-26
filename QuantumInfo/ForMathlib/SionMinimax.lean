905e01b6249e7b782ae5ee991023148a6bddcb97
Dennj Osele <dennj.osele@gmail.com>
2026-03-26 18:50:01 +0000

    Fix all non-sorry warnings

--- QuantumInfo/ForMathlib/SionMinimax.lean
+++ QuantumInfo/ForMathlib/SionMinimax.lean
@@ -8,7 +8,10 @@ import Mathlib.Analysis.Convex.Quasiconvex
 import Mathlib.Analysis.Convex.Topology
 import Mathlib.Analysis.Normed.Order.Lattice
 import Mathlib.Data.Fintype.Order
-import Mathlib.Topology.Semicontinuous
+import Mathlib.Algebra.Order.Module.Field
+import Mathlib.Data.EReal.Operations
+import Mathlib.Topology.Algebra.InfiniteSum.Order
+import Mathlib.Topology.MetricSpace.Bounded
 import Mathlib.Analysis.SpecificLimits.Basic
 
 @[simp]
